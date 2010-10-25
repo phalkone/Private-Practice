a30e1f97f702aca4c66901ef4e158b48da7cda1e
Tim Pintens <phalkone@yahoo.com>
2010-10-25 14:55:51 +0000

    General i18n settings and jquery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_locale 
+
+  def set_locale 
+    I18n.locale = extract_locale_from_subdomain
+  end
+
+  def extract_locale_from_subdomain
+    parsed_locale = request.subdomains.first
+    if !parsed_locale.nil?
+      I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module PrivatePractice
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')] 
+    config.i18n.default_locale = :nl
 
     # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+    config.action_view.javascript_expansions[:defaults] = %w(jquery-1.4.1 rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
