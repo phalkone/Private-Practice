2c231c1b244511ba8cb6eba42e19c23e29bd5764
Tim Pintens <phalkone@yahoo.com>
2010-10-31 14:29:17 +0000

    Finished create for Page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,8 +1,20 @@
 class Page < ActiveRecord::Base
-  attr_accessible :title, :body, :locale
+  attr_accessible :title, :body, :locale, :nested
 
-  validates_presence_of :title, :body, :permalink, :locale
+  validates_presence_of :title, :body, :locale
   validates_length_of :title, :maximum=>30
   validates_length_of :locale, :maximum=>2
   validates_length_of :body, :maximum=>10000
+
+  def before_create
+    @attributes['permalink'] = 
+      title.downcase.gsub(/\s+/, '_').gsub(/[^a-zA-Z0-9_]+/, '')
+    if Page.where("locale = ?",locale).count > 0 
+      @last = Page.where("locale = ?",locale).order("sequence ASC").last
+      @attributes['sequence'] = @last.sequence + 10
+    else
+      @attributes['sequence'] = 10
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101029175525) do
+ActiveRecord::Schema.define(:version => 20101031141638) do
 
   create_table "pages", :force => true do |t|
     t.string   "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(:version => 20101029175525) do
     t.string   "locale"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "sequence"
+    t.boolean  "nested"
   end
 
 end
