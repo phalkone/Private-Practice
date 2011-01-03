3dc0a3ad96371b60a204445306265c14dbbf156d
Tim Pintens <phalkone@yahoo.com>
2011-01-03 09:16:52 +0000

    Finished appointments CRUD

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,18 +3,17 @@ class AppointmentsController < ApplicationController
  
   def index
     @title = t("appointments.menutitle")
-    @date = Date.new(2010, 12, 20)
-    @start = 6
-    @stop = 18
-    @begin = Time.new(@date.year,@date.month,@date.day,@start,0,0, Time.now.utc_offset)
-    @end = Time.new(@date.year,@date.month,@date.day,@stop,0,0, Time.now.utc_offset)
-    @appointments = current_user.appointments.where("begin >= ? AND end <= ?", @begin , @end).order("begin ASC")
-    @dayarray = dayarray(@start, @stop, @appointments)
-    @maxcols = @dayarray.last[0];
+    setValues
+  end
+
+  def show
+    @appointment = Appointment.find(params[:id])
   end
 
   def new
-     @appointment = Appointment.new
+    @appointment = Appointment.new
+    @title = t("appointments.newapp")
+    render "open"
   end
 
   def create
@@ -22,18 +21,30 @@ class AppointmentsController < ApplicationController
     if @appointment.save
       flash.now[:notice] = t("appointments.created")
     end
+    render "save"
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+    @appointment.date = l(@appointment.begin, :format => :datepicker)
+    @appointment.begin_time = l(@appointment.begin.to_time, :format => :time)
+    @appointment.end_time = l(@appointment.end.to_time, :format => :time)
+    @begin_slider = (@appointment.begin.hour * 60) + @appointment.begin.min
+    @end_slider = (@appointment.end.hour * 60) + @appointment.end.min
+    @title = t("appointments.edit")
+    render "open"
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.update_attributes(params[:appointment])
+      flash.now[:notice] = t("appointments.updated")
+    end
+    render "save"
   end
 
   def refresh
-    datesplit = params["view"]["date"].split(" ")
-    @date = Date.new(datesplit[0].to_i,datesplit[1].to_i,datesplit[2].to_i)
-    @start = params["view"]["start"].to_i
-    @stop = params["view"]["stop"].to_i
-    @begin = Time.new(@date.year,@date.month,@date.day,@start,0,0, Time.now.utc_offset)
-    @end = Time.new(@date.year,@date.month,@date.day,@stop,0,0, Time.now.utc_offset)
-    @appointments = current_user.appointments.where("begin >= ? AND end <= ?", @begin , @end).order("begin ASC")
-    @dayarray = dayarray(@start, @stop, @appointments)
-    @maxcols = @dayarray.last[0];
+    setValues
   end
 
   def destroy
@@ -46,4 +57,29 @@ class AppointmentsController < ApplicationController
     def authenticate
       deny_access unless role?("doctor")
     end
+
+   def setValues
+     if params["view"] 
+       datesplit = params["view"]["date"].split(" ")
+       @date = Date.new(datesplit[0].to_i,datesplit[1].to_i,datesplit[2].to_i)
+       @start = params["view"]["start"].to_i
+       @stop = params["view"]["stop"].to_i
+       @dayweek = params["view"]["dayweek"]
+     else
+       @date = Date.new(2010, 12, 20)
+       @start = 6
+       @stop = 18
+       @dayweek = "day"
+     end
+     @begin = Time.new(@date.year,@date.month,@date.day,@start,0,0, Time.now.utc_offset)
+     @end = Time.new(@date.year,@date.month,@date.day,@stop,0,0, Time.now.utc_offset)
+     @appointments = current_user.appointments.where("begin >= ? AND begin <= ?", @begin , @end).order("begin ASC")
+     if (@appointments.count != 0) && (@appointments.last.end > @end)
+       @stop = @appointments.last.end.hour + 1
+     end
+     if @dayweek == "day"
+       @dayarray = dayarray(@start, @stop, @appointments)
+       @maxcols = @dayarray.last[0];
+    end
+  end
 end
