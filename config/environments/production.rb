5706ef0cb251998b5fa4a98919c0e6b643faefc3
Tim Pintens <phalkone@yahoo.com>
2011-09-04 17:38:35 +0200

    Ammended production configuration for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,13 +9,13 @@ PrivatePractice::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -24,6 +24,7 @@ PrivatePractice::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Specifies the header that your server uses for sending files
+  config.action_dispatch.x_sendfile_header = nil
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
@@ -43,7 +44,7 @@ PrivatePractice::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( blueprint/ie.css blueprint/print.css blueprint/screen.css jquery/jquery.ui.all.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
