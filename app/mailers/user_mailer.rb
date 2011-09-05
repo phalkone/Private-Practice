3c52e8b39a35bc3af17030872a3fed18e6aba543
Tim Pintens <phalkone@yahoo.com>
2011-09-05 14:20:31 +0200

    Adjusted mailer to new locale detection

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,10 @@
 class UserMailer < ActionMailer::Base
   default :from => "webmaster@app.local"
+  # TODO Set real host for production
+  default_url_options[:host] = "localhost:3000"
   
   def contact_message(message)
-    @url = message_url(message, :host => I18n.locale.to_s + ".app.local:3000")
+    @url = message_url(message, :locale => I18n.locale.to_s)
     mail(:from => message.email, :to => message.user.email,
       :subject => message.subject) do |format|
         format.text
@@ -11,8 +13,8 @@ class UserMailer < ActionMailer::Base
   
   def email_confirmation_instructions(user)
     @user = user
-    @url  = email_confirmation_url(user.perishable_token, :host => I18n.locale.to_s + ".app.local:3000")
-    @url2 = new_email_confirmation_url(:host => I18n.locale.to_s + ".app.local:3000", :email => user.email)
+    @url  = email_confirmation_url(:id => user.perishable_token, :locale => I18n.locale.to_s)
+    @url2 = new_email_confirmation_url(:email => user.email, :locale => I18n.locale.to_s)
     mail(:to => user.email,
       :subject => t("email_confirmation_instructions.title"))  do |format|
         format.html
@@ -22,7 +24,7 @@ class UserMailer < ActionMailer::Base
 
   def password_reset_instructions(user)
     @user = user
-    @url  = edit_password_reset_url(user.perishable_token, :host => I18n.locale.to_s + ".app.local:3000")
+    @url  = edit_password_reset_url(user.perishable_token, :locale => I18n.locale.to_s)
     mail(:to => user.email,
       :subject => t("password_reset_instructions.title")) do |format|
         format.html
